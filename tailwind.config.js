c4191d5a210aa1815fb2b313c9cda75175901f8d
zKazemeini125 <kazemeiniz125@gmail.com>
2024-12-02 12:57:35 +0330

    Create tic tac toe and set for mobile devices

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      lightGray: "#282828",
+      grid: "#3C3C3C",
+      cells:"#444444",
+      txtColor:"#7b7b7b",
+      XColor:"#FF4500",
+      OColor:"#00CED1"
+    },
+  },
+  plugins: [],
+};
