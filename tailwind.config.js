7813df1e30def4add24ea9c343e03c8b1bf273ea
zKazemeini125 <kazemeiniz125@gmaol.com>
2025-07-11 13:53:29 +0330

    Add modal colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,19 @@ export default {
   theme: {
     extend: {},
     colors: {
-      lightGray: "#282828",
-      modalColor:"#424242c0",
+      //lightGray: "#282828",
+      modalBackgroundColor:"#424242c0",
+      modalBoxReceiveInformation:"#2b2b2bc0",
+      modalTextColor:"#e0e0e0",
+      modalInputBackgroundcolor:"#676767",
       grid: "#3C3C3C",
       cells:"#444444",
       txtColor:"#7b7b7b",
       txtColorSharp:"#d0d0d0" ,
       XColor:"#FF4500",
       OColor:"#00CED1",
-      red:"#ff0000"
+      red:"#ff0000",
+      black:"#000000"
     },
   },
   plugins: [],
