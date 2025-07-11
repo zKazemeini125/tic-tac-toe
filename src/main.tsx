717937d5e2e310221c76e7f9cb9e836e5cd26fca
zKazemeini125 <kazemeiniz125@gmaol.com>
2025-07-11 13:56:55 +0330

    Return to the main program mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 //import App from './App.tsx'
-import Startup from './startup/Startup.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Startup />
+    <App/>
   </StrictMode>,
 )
